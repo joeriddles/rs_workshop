11255b2ccda3b24a567523c45ddb7fc216f9f4dd
joeriddles <joeriddles10@gmail.com>
2024-05-09 18:59:53 -0700

    Implement CLI

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# Spokane Rust User Group Workshop 2024.05.09
+
+Steps
+```shell
 cargo new rs_workshop
 cargo run
 cargo add clap --features derive
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    Add(Arguments),
+    Sub(Arguments),
+    Mul(Arguments),
+    Div(Arguments),
+}
+
+#[derive(Debug, Args)]
+struct Arguments {
+    lhs: f32,
+    rhs: f32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    let answer;
+    match cli.command {
+        Command::Add(args) => {
+            answer = args.lhs + args.rhs;
+        }
+        Command::Sub(args) => {
+            answer = args.lhs - args.rhs;
+        }
+        Command::Mul(args) => {
+            answer = args.lhs * args.rhs;
+        }
+        Command::Div(args) => {
+            answer = args.lhs / args.rhs;
+        }
+    }
+    println!("{answer}");
 }
